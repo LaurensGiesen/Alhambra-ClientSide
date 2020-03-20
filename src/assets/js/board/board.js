f0399a98f637f32fbb3598233721fb9c88f20993
kwinten <kwinten.tas@student.howest.be>
2020-03-20 13:38:40 +0100

    html of board grid toevoegen aan board html of alhambra grid alhambra html/CSS of tiles

---
+++ src/assets/js/board/board.js
@@ -0,0 +1,37 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded',init);
+
+function init(){
+    scrollToFountain();
+    document.addEventListener('keydown', shortcuts);
+}
+
+function scrollToFountain() {
+    document.querySelector(".row0").scrollIntoView({
+        behavior: 'auto',
+        block: 'center',
+        inline: 'center'
+    });
+}
+
+function shortcuts(e){
+    let alh = document.querySelector("#alhambra");
+    let tileHeight = document.querySelector(".row0").clientHeight;
+    e.preventDefault();
+    switch(e.keyCode){
+        case 37: //ArrowLeft
+            alh.scrollBy(tileHeight*-1,0);
+            console.log(tileHeight);
+            break;
+        case 38: //ArrowUp
+            alh.scrollBy(0, tileHeight*-1);
+            break;
+        case 39: //ArrowRight
+            alh.scrollBy(parseInt(tileHeight),0);
+            break;
+        case 40: //ArrowDown
+            alh.scrollBy(0,parseInt(tileHeight));
+            break;
+    }
+}
