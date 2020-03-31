4dac072bfda1b0eda02c4e79890ef1ae31a34e3f
kwinten <kwinten.tas@student.howest.be>
2020-03-31 14:33:06 +0200

    refactor JS of gamelobby

--- src/assets/js/functions/player.js
+++ src/assets/js/functions/player.js
@@ -8,3 +8,15 @@ function validatePlayerName(playerName){
     //check if only lowercase or numbers, max lenght is 20 and not empty
     return (/^[a-z0-9]*$/).test(playerName) && playerName.length <= config.maxLengthPlayerName && playerName.length > 0;
 }
+
+function setPlayerStatusReady(callback){
+    fetchFromServer(`${config.root}games/${_gameId}/players/${_playerName}/ready`, 'PUT').then(function (response) {
+        callback(response);
+    });
+}
+
+function setPlayerStatusNotReady(callback){
+    fetchFromServer(`${config.root}games/${_gameId}/players/${_playerName}/ready`, 'DELETE').then(function (response) {
+        callback(response);
+    });
+}
