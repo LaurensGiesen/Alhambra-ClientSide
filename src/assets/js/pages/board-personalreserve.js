e6e865fd80f964fc95717f254774b8ed9e23fb66
Jonas Herteleer <jonas.herteleer@student.howest.be>
2020-04-08 14:35:09 +0200

    Dynamisch inladen van personalreserve done helemaal !

--- src/assets/js/pages/board-personalreserve.js
+++ src/assets/js/pages/board-personalreserve.js
@@ -5,18 +5,16 @@ let personalReserveHTML = document.querySelector("#personalreserve div");
 function pollPersonalReserve(){
     setTimeout(pollPersonalReserve, config.pollingTime);
 
-    let playertjeeeeee = _gameAuth.players;
+    let player = _gameAuth.players;
 
-    console.log(playertjeeeeee);
 
-    for (let i = 0; i < playertjeeeeee.length; i++) {
-        console.log(playertjeeeeee[i].name);
-        if (playertjeeeeee[i].name === _playerName) {
-            let kenkl = playertjeeeeee[i].reserve;
+    for (let i = 0; i < player.length; i++) {
+        if (player[i].name === _playerName) {
+            let personalReserve = player[i].reserve;
             personalReserveHTML.innerHTML = "";
-
-            for(let i = 0; i < kenkl.length; i++) {
-                let reserve = kenkl[i];
+            console.log(personalReserve);
+            for(let i = 0; i < personalReserve.length; i++) {
+                let reserve = personalReserve[i];
                 personalReserveHTML.innerHTML += `
                 <article class="tile ${reserve.type}" data-type="${reserve.type}" data-cost="${reserve.cost}" data-walls="${reserve.walls}">
                     <img src="../assets/media/icons/${reserve.type}.png" alt="${reserve.type}" draggable="false">
