601a0f7641a416585981b8e520e9a8d740d48582
kwinten <kwinten.tas@student.howest.be>
2020-05-12 10:44:13 +0200

    change root to own API

--- src/assets/js/functions/config.js
+++ src/assets/js/functions/config.js
@@ -2,8 +2,8 @@
 // Feel free to extend this object with further configurables for your application
 
 const config = {
-    root: "http://172.21.22.52:48201/alhambra-api/",
-    groupnumber: "20",
+    root: "http://172.21.22.52:48201/alhambra-20/api/",
+    //groupnumber: "20",
     maxLengthPlayerName: 20,
     towersOnMap: 12,
     maxNumberOfParticipants: 6,
